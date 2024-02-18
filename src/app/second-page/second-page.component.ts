7f43ee8843437eb850b166f8226400c95930dbf7
Sartaj Singh <sin14136@sheridancollege.ca>
2024-02-17 22:27:27 -0500

    second page created

--- src/app/second-page/second-page.component.ts
+++ src/app/second-page/second-page.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second-page',
   templateUrl: './second-page.component.html',
-  styleUrl: './second-page.component.css'
+  styleUrls: ['./second-page.component.css']
 })
 export class SecondPageComponent {
+  selectedFlavor!: string;
+  subtotal!: number;
+  quantity!: number;
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.quantity = params['quantity'];
+      this.subtotal = params['quantity'] * 2;
+    });
+  }
+
+  goToFirstPage() {
+    this.router.navigate(['/']).then(success => {
+      if (success) {
+        console.log('Navigation to First Page successful');
+      } else {
+        console.error('Navigation to First Page failed');
+      }
+    });
+  }
+
+  goToThirdPage() {
+    this.router.navigate(['/third-page'], { queryParams: { quantity: this.quantity,flavor: this.selectedFlavor, subtotal: this.subtotal } }).then(success => {
+      if (success) {
+        console.log('Navigation to Third Page successful');
+      } else {
+        console.error('Navigation to Third Page failed');
+      }
+    });
+  }
 }
