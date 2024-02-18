f408e02cfff97d5050f75e24c51356b2f02240c9
Sartaj Singh <sin14136@sheridancollege.ca>
2024-02-17 22:09:52 -0500

    first page created

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
-  styleUrl: './first-page.component.css'
+  styleUrls: ['./first-page.component.css']
 })
 export class FirstPageComponent {
+  constructor(private router: Router) { }
 
+  goToSecondPage(quantity: number) {
+    this.router.navigate(['/second-page'], { queryParams: { quantity } }).then(success => {
+      if (success) {
+        console.log('Navigation to Second Page successful');
+      } else {
+        console.error('Navigation to Second Page failed');
+      }
+    });
+  }
 }
